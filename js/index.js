760d7ed3e4c24357b89f2612457e68910d5b11ee
Gilberts Ahumada Cabrera <gilbertsahumada@gmail.com>
2018-07-23 22:31:38 +1200

    first commit to second page

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(document).ready({
+
+});
+
+$(".nv-txt").click(function(){
+    $(".nv-txt").removeClass('active')
+    $(this).addClass('active')
+});
+
+$("#btnRmHome").click(function(){
+
+    $("#exampleModal").modal('show')
+})
+
